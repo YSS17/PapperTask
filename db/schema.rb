f440141732443697c6a17cc0d1bb0e413b7a08de
Yan Santos <yan17souza@hotmail.com>
2022-06-17 17:16:56 -0300

    adding projects model and running scaffold also fixing some migration problems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_195417) do
+ActiveRecord::Schema.define(version: 2022_06_17_200524) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "team_id"
+    t.integer "user_id"
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
