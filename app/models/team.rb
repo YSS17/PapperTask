f04c97ae7b9b6c5c2b344e03d1866ff1772d510d
Yan Santos <yan17souza@hotmail.com>
2022-06-17 18:40:14 -0300

    fixing home controller & adding basic view to dashboard

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,5 @@ class Team < ApplicationRecord
   has_many :projects, dependent: :destroy # if the project gets deleted the team gets too
   has_many :users
 
-  accept_nested_attributes_for :users, allow_destroy: true
+  accepts_nested_attributes_for :users, allow_destroy: true
 end
