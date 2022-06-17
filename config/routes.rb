5e4a42e92d0ec82c8339f378e73d0135c5879896
Yan Santos <yan17souza@hotmail.com>
2022-06-17 12:07:11 -0300

    fixing filename error in registrations_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-
+  
   as :user do
     put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
   end
 
-  devise_for :users, controller: {
-    ragistrations: 'ragistrations',
-    confirmation: 'confirmations'
+   devise_for :users, controllers: {
+  	registrations: 'registrations',
+  	confirmations: 'confirmations'
   }
   
   root 'home#index'
